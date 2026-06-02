10d35a63c1a41a33fcb226bbe9f1404eaaee9bf3
naveengadda86-gif <naveengadda86@gmail.com>
2026-06-02 20:53:32 +0530

    updates

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+from flask import Flask, render_template
 
 from database.connection import create_tables
 from routes.user_routes import register_user_routes
@@ -10,12 +11,22 @@ from routes.expense_routes import register_expense_routes
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
 app.config["JWT_SECRET_KEY"] = "mysecretkey123"
 
 jwt = JWTManager(app)
 
 # Create tables on startup
 create_tables()
+
+
 
 # Register all routes
 register_user_routes(app)
